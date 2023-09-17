b40ba09286628ffd3e45b172aadb2e88566bdb8d
Arjun Singh <ajsinghyadav00@gmail.com>
2023-09-17 18:51:35 +0530

    some update
    Signed-off-by: Arjun Singh <ajsinghyadav00@gmail.com>

--- fine-tuning/openAI.py
+++ fine-tuning/openAI.py
@@ -3,19 +3,15 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#response = openai.File.create(
-#  file=open("ldap.jsonl", "rb"),
-#  purpose='fine-tune'
-#)
+response = openai.File.create(
+  file=open("data.jsonl", "rb"),
+  purpose='fine-tune'
+)
 
-#response = openai.FineTuningJob.create(training_file="file-yaQpWsd5DDzT9esj6dPq7AXs", model="gpt-3.5-turbo")
+response = openai.FineTuningJob.create(training_file="file-VXDnyJn14ejnKVQyEdUvYgvD", model="ft:gpt-3.5-turbo-0613:0x34d::7yaoKJRt")
 
+response = openai.FineTuningJob.retrieve("ftjob-RL1Yjc2M8mDrdzGhEbN2dlCW")
 
-# Retrieve the state of a fine-tune
-#response = openai.FineTuningJob.retrieve("ftjob-4ZFxFOjEMJhpFqKyetHh3F4O")
-
-response = openai.FineTuningJob.list_events(id="ftjob-4ZFxFOjEMJhpFqKyetHh3F4O", limit=10)
+response = openai.Model.delete("ft:gpt-3.5-turbo-0613:0x34d::7yZLjRMZ")
 
 print(response)
-
-#ft:gpt-3.5-turbo-0613:0x34d::7yZLjRMZ
--- gpt-engine.go
+++ gpt-engine.go
@@ -10,19 +10,32 @@ import (
 )
 
 var APIKEY = os.Getenv("OPENAI_API_KEY")
-var SYSTEM_AI = `You are a Golang fuzzing expert, check if this function is worth fuzzing.`
+var SYSTEM_AI = `
+You are a Golang fuzzing expert. Given a function name and number of arguments, determine whether the function is worth fuzzing. Answer Yes or No.
+
+Here are some general guidelines:
+	* Functions with names like Add, Remove, Get, Set, Update, Delete, Save, Call, and New are usually internal APIs and are not good targets for fuzzing.
+	* Functions with a lot of arguments are also not good for fuzzing.
+	* However, there are exceptions to these guidelines. For example, a function called ParseRequest() might be a good target for fuzzing, even though it has a generic name.
+	* Ultimately, the best way to determine whether a function is worth fuzzing is to consider its specific implementation and the types of inputs that it accepts.
+	* Function that call a network, or A database request consider them as a bad request, because for them i need to spin a network fuzzer.
+	* Don't use function that's call offical golang library like : json,bas64,regexp
+
+Example queries:
+	* Is the function ParseRequest() worth fuzzing? (Answer: Yes)
+	* Is the function Add() worth fuzzing? (Answer: No)
+	* Is the function SaveUser() worth fuzzing? (Answer: Maybe, depending on its implementation)
+	* Please answer the following query:
+`
 
 func GPTWork(funcName string, functions string, tests string) {
-
-	fmt.Println(BlueColor + strings.Repeat("+", 80) + "" + ResetColor)
-	fmt.Println(RedColor + "Function Name:" + funcName + ResetColor)
 
 	client := openai.NewClient(APIKEY)
 
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: "ft:gpt-3.5-turbo-0613:0x34d::7yZLjRMZ",
+			Model: "gpt-4",
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleSystem,
@@ -38,33 +51,19 @@ func GPTWork(funcName string, functions string, tests string) {
 	)
 
 	if err != nil {
+		fmt.Println(BlueColor + strings.Repeat("+", 80) + "" + ResetColor)
+		fmt.Println(RedColor + "Function Name: " + funcName + ResetColor)
 		fmt.Printf(CyanColor+"ChatCompletion error: %v\n"+ResetColor, err)
-	} else {
-		fmt.Println(CyanColor + resp.Choices[0].Message.Content + ResetColor)
-	}
-
-	fmt.Println(BlueColor + strings.Repeat("-", 80) + "\n" + ResetColor)
-}
-
-/*
-	gptinput := functions + "\n\n\n" + tests
-	jsonString, _ := json.Marshal(gptinput)
-	fmt.Println(BlueColor + "GPT-3 Input: " + ResetColor + string(gptinput))
-	fmt.Println(BlueColor + "GPT-3 Input: " + ResetColor + string(jsonString))
-*/
-
-/*
-	if counter == 2 {
-		return true
-	}
-	if len(funcDecls[0].Type.Params.List) > 2 {
+		fmt.Println(BlueColor + strings.Repeat("-", 80) + "\n" + ResetColor)
 		return
 	}
-	Blacklist := []string{"Add", "Remove", "Get", "Set", "Update", "Delete", "Save", "Call", "New"}
-	for _, name := range Blacklist {
-		if strings.Contains(strings.ToLower(funcName), strings.ToLower(name)) {
-			fmt.Println(RedColor + "Blacklist name: `" + funcName + "`\n" + ResetColor)
-			return
-		}
+
+	if resp.Choices[0].Message.Content == "No" {
+		return
 	}
-*/
+
+	fmt.Println(BlueColor + strings.Repeat("+", 80) + "" + ResetColor)
+	fmt.Println(RedColor + "Function Name: " + funcName + ResetColor)
+	fmt.Println(CyanColor + resp.Choices[0].Message.Content + ResetColor)
+	fmt.Println(BlueColor + strings.Repeat("-", 80) + "\n" + ResetColor)
+}
